221676a1e4b5d2274a617da7c2cf95cef780941a
fairuuz <justcall1313@gmail.com>
2026-01-28 17:31:08 +0700

    fix error for deploy

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -7,6 +7,8 @@ export const config = {
     NODE_ENV: process.env.NODE_ENV || 'development',
     DATABASE_URL: process.env.DATABASE_URL,
     JWT_SECRET: process.env.JWT_SECRET || "123456",
+    GEMINI_API_KEY: process.env.GEMINI_API_KEY,
+    BREVO_API_KEY: process.env.BREVO_API_KEY,
 } as const;
 
 export default config;
